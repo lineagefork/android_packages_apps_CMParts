be823cafd964456d0c9ac3d15b80f6e6a8574cb4
Arnav Gosain <arn4v@outlook.com>
2017-05-08 17:19:55 +0530

    shift class for android settings provider to fix fcs
    Change-Id: I15e0664e5864cc71313751a34be6b105e4816797

--- src/org/cyanogenmod/cmparts/fork/ForkCustomization.java
+++ src/org/cyanogenmod/cmparts/fork/ForkCustomization.java
@@ -15,17 +15,16 @@ import android.preference.SwitchPreference;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import cyanogenmod.preference.CMSecureSettingSwitchPreference;
+import cyanogenmod.preference.SecureSettingSwitchPreference;
 import cyanogenmod.providers.CMSettings;
 import org.cyanogenmod.cmparts.R;
 import org.cyanogenmod.cmparts.SettingsPreferenceFragment;
 
 public class ForkCustomization extends SettingsPreferenceFragment {
-    private static final String TAG = "ForkCustomization";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.fork_customization);
     }
 }
