16779730dd9c77732e2acffc836dcb93cbfa9cce
Arnav Gosain <arn4v@outlook.com>
2017-07-13 18:10:10 +0530

    Recents FAB Clear All Button Tweaks [2/2]
    From cristianomatos
    * Show or hide recents button
    * Place the button in top left, center, right or bottom left, center, right
    
    Change-Id: I298f24ab936a9bf272f00474a7cde42610e8345d

--- src/org/cyanogenmod/cmparts/fork/ForkCustomization.java
+++ src/org/cyanogenmod/cmparts/fork/ForkCustomization.java
@@ -5,6 +5,7 @@ import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
@@ -20,11 +21,40 @@ import cyanogenmod.providers.CMSettings;
 import org.cyanogenmod.cmparts.R;
 import org.cyanogenmod.cmparts.SettingsPreferenceFragment;
 
-public class ForkCustomization extends SettingsPreferenceFragment {
+public class ForkCustomization extends SettingsPreferenceFragment
+        implements OnPreferenceChangeListener {
+
+    private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+    private ListPreference mRecentsClearAllLocation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.fork_customization);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mRecentsClearAllLocation = (ListPreference) findPreference(RECENTS_CLEAR_ALL_LOCATION);
+        int location = Settings.System.getInt(resolver,
+                Settings.System.RECENTS_CLEAR_ALL_LOCATION, 3);
+        mRecentsClearAllLocation.setValue(String.valueOf(location));
+        mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
+        mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mRecentsClearAllLocation) {
+            int location = Integer.valueOf((String) newValue);
+            int index = mRecentsClearAllLocation.findIndexOfValue((String) newValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.RECENTS_CLEAR_ALL_LOCATION, location);
+            mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
+            return true;
+        }
+        return false;
     }
 }
